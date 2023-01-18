288b428e6a1c86076b8081b716113db6442a5f51
gabriel lencina <gabriellencina831@gmail.com>
2023-01-17 22:27:17 -0300

    inicio da sessão de strings

---
+++ interpolando_variáveis/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = "Gabriel";
+$idade = 21;
+
+
+echo "Eu me chamo $nome tenho $idade anos <br>";
+
+echo "Eu me chamo {$nome} tenho {$idade} anos <br>";
---
+++ valores_escape/index.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Content-type: text/plain");
+
+// pular  linha
+echo "Isso aqui vai pular linha \n e isso segunda linha \n";
+
+// Tab
+echo "Testando o tab \t aqui \n";
+
+// Barra invertida
+echo "Barra invertida \\ \n";
+
+// Dólar
+echo "imprimindo o dolar \$teste";
